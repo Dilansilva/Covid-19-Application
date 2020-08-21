d92fb4b582378ec0c303f6de1b5dddb28a52fcf2
Dilan Silva <sdilansilva@gmail.com>
2020-08-21 15:45:56 +0530

    Done some style sheets and call a function when page load

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,19 @@
-console.log('Hello World');
 
-const covidForm = document.querySelector('form');   
-const search = document.querySelector('input');
-const confirmed = document.querySelector('#confirmed');
+
+const covidForm = document.querySelector('form');   //for form
+const search = document.querySelector('input');     //for search country
+const confirmed = document.querySelector('#confirmed');     
 const recovered = document.querySelector('#recovered');
 const active = document.querySelector('#active');
 const country = document.querySelector('#country');
-
-
-
+ 
+document.addEventListener("DOMContentLoaded", () => { //This function calls when page load
+    console.log('Hello World');
+});
 
 covidForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const location = search.value;
+    const location =  search.value;
     
     fetch('http://localhost:3000/covid?search='+location+'').then((response) => {
         response.json().then( (data) => {
